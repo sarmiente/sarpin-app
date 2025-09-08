1a94f39c77cefaf5442a8f4c96bbc63bb6d4d907
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-09-08 16:50:04 -0500

    Contenido nuevo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true
---
+++ site.config.ts
@@ -0,0 +1,4 @@
+export const siteUrl = "https://sarpin.agency/";
+export const siteTitle = "Sarpin Agency"
+export const siteDescription = "Mejora la visibilidad de tu negocio con un sitio web que capture la esencia de tu negocio.";
+
